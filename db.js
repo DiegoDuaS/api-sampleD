fecbba077a285985e1e54f9146129ebdaeab2522
DiegoDSlo <diego.slowing@gmail.com>
2024-03-07 00:04:37 -0600

    fix: errors corrected

--- db.js
+++ db.js
@@ -1,24 +1,26 @@
 import conn from './conn.js'
 
 export async function getAllPosts() {
- const [rows] = await conn.query('SELECT * FROM blog_posts')
- return rows
+  const [rows] = await conn.query('SELECT * FROM blog_posts')
+  return rows
 }
 
 export async function createPost(country, capital_city, size, off_language, PIB) {
-   const [result] = await conn.query('INSERT INTO blog_posts (country, capital_city, size, off_language, PIB) VALUES (?, ?, ?, ?, ?)', [country, capital_city, size, off_language, PIB])
-   return result
+  const [result] = await conn.query('INSERT INTO blog_posts (country, capital_city, size, off_language, PIB) VALUES (?, ?, ?, ?, ?)', [country, capital_city, size, off_language, PIB])
+  return result
 }
 
 export async function getPost(id) {
-   const [result] = await conn.query('SELECT * FROM blog_posts WHERE id = ?', [id])
+  const [result] = await conn.query('SELECT * FROM blog_posts WHERE id = ?', [id])
+  return result
 }
 
 export async function deletePost(id) {
-   const [result] = await conn.query('DELETE FROM blog_posts WHERE id = ?', [id])
+  const [result] = await conn.query('DELETE FROM blog_posts WHERE id = ?', [id])
+  return result
 }
 
 export async function modifyPost(id, columna, dato) {
-   const [result] = await conn.query(`UPDATE blog_posts SET ${columna} = ? WHERE id = ?`, [dato, id])
-   return result
+  const [result] = await conn.query(`UPDATE blog_posts SET ${columna} = ? WHERE id = ?`, [dato, id])
+  return result
 }
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getAllPosts } from './db.js'
-import { createPost } from './db.js'
-import { getPost } from './db.js'
-import { deletePost } from './db.js'
-import { modifyPost } from './db.js'
+import {
+  getAllPosts,
+  createPost,
+  getPost,
+  deletePost,
+  modifyPost,
+} from './db.js';
 
 const app = express()
 app.use(express.json())
@@ -11,64 +13,68 @@ const port = 3000
 
 app.get('/posts', async (req, res) => {
   try {
-      const allPosts = await getAllPosts()
-      res.json(allPosts)
+    const { allPosts } = await getAllPosts()
+    res.json(allPosts)
   } catch (error) {
-      console.error('Error al obtener todos los posts:', error)
-      res.status(500).json({ error: 'Error al obtener todos los posts' })
+    console.error('Error al obtener todos los posts:', error)
+    res.status(500).json({ error: 'Error al obtener todos los posts' })
   }
 })
 
 app.post('/posts', async (req, res) => {
-  const { country, capital_city, size, off_language, PIB } = req.body
+  const {
+    country,
+    capital_city,
+    size,
+    off_language,
+    PIB,
+  } = req.body;
   try {
-      const result = await createPost(country, capital_city, size, off_language, PIB)
-      res.json(result)
+    const result = await createPost(country, capital_city, size, off_language, PIB)
+    res.json(result)
   } catch (error) {
-      console.error('Error al crear un nuevo post:', error)
-      res.status(500).json({ error: 'Error al crear un nuevo post' })
+    console.error('Error al crear un nuevo post:', error)
+    res.status(500).json({ error: 'Error al crear un nuevo post' })
   }
 })
 
-
 app.get('/posts/:id', async (req, res) => {
-  const id = req.params.id
+  const { id } = req.params.id
   try {
-      const post = await getPost(id)
-      res.json(post)
+    const post = await getPost(id)
+    res.json(post)
   } catch (error) {
-      console.error('Error al obtener el post:', error)
-      res.status(500).json({ error: 'Error al obtener el post' })
+    console.error('Error al obtener el post:', error)
+    res.status(500).json({ error: 'Error al obtener el post' })
   }
 })
 
 app.delete('/posts/:id', async (req, res) => {
-  const id = req.params.id
+  const { id } = req.params.id
   try {
-      const result = await deletePost(id)
-      res.json(result)
+    const result = await deletePost(id)
+    res.json(result)
   } catch (error) {
-      console.error('Error al eliminar el post:', error)
-      res.status(500).json({ error: 'Error al eliminar el post' })
+    console.error('Error al eliminar el post:', error)
+    res.status(500).json({ error: 'Error al eliminar el post' })
   }
 })
 
 app.put('/posts/:id', async (req, res) => {
-  const id = req.params.id
+  const { id } = req.params;
   const { columna, dato } = req.query
   try {
-      const result = await modifyPost(id, columna, dato)
-      res.json(result)
+    const result = await modifyPost(id, columna, dato)
+    res.json(result)
   } catch (error) {
-      console.error('Error al modificar el post:', error)
-      res.status(500).json({ error: 'Error al modificar el post' })
+    console.error('Error al modificar el post:', error)
+    res.status(500).json({ error: 'Error al modificar el post' })
   }
 })
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(501).json({ error: 'Método HTTP no implementado' })
 })
-
 
 app.listen(port, '127.0.0.1', () => {
   console.log(`Server listening at http://127.0.0.1:${port}`)
