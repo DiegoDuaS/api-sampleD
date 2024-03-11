a3369ca3bbd1b666447f337a063e60a6869fd2c7
DiegoDSlo <diego.slowing@gmail.com>
2024-03-11 16:57:08 -0600

    feat: changed the database to upload to server

--- src/conn.js
+++ src/conn.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'DiegoDS',
-  database: 'blog_db',
+  database: 'blog_diegods',
   password: 'Dd425',
   waitForConnections: true,
   connectionLimit: 10,
