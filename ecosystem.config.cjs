3140ba3a13aec4be6cb99938997169d35c33d636
DiegoDSlo <diego.slowing@gmail.com>
2024-03-11 17:50:50 -0600

    feat: added ecosystem.config.cjs file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: 'blog_diegods',
+        script: 'npm',
+        args: 'start',
+        log_file: './blog_diegods.txt',
+        merge_logs: true
+    }]
+}
